43346031be2d87e22e88c62f9a74b0d532c37a06
HAMADSAALEH <hamadsalehswabi@gmail.com>
2023-10-22 17:11:14 +0500

    TAXES.C 1

--- taxes.c
+++ taxes.c
@@ -14,26 +14,3 @@ int main(int argc, char **argv) {
   double childCredit = 0.0;
   double totalTax = 0.0;
   int numChildren = 0;
-
-  printf("Please enter your adjusted gross income (AGI): ");
-  scanf("%lf", &agi);
-  //remove the "enter" endline character
-  getchar(); 
-
-  printf("Do you have any children? (Y) or (N)? ");
-  c = getchar();
-
-  if(c == 'y' || c == 'Y') {
-    printf("How many children do you have? ");
-    scanf("%d", &numChildren);
-  }
-
-  //TODO: compute the tax, child credit, and total tax here
-
-  printf("AGI:          $%10.2f\n", agi);
-  printf("Tax:          $%10.2f\n", tax);
-  printf("Child Credit: $%10.2f\n", childCredit);
-  printf("Total Tax:    $%10.2f\n", totalTax);
-
-  return 0;
-}
